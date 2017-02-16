9c1e3eacfbf58b577b5d422387aab5c4e42ea707
ravilkg <ravil.kirgizkhodzhaev@gmail.com>
2017-02-16 15:06:49 +0600

    separate main script

--- bot.py
+++ bot.py
@@ -48,19 +48,19 @@ def send_message(chat_id, text = 'Wait a second please...'):
     requests.get(url)
 
 
-def main():
-    while True:
-        answer = get_message()
-
-        if answer != None:
-            chat_id = answer['chat_id']
-            text = answer['text']
-
-            if text == '/btc':
-                send_message(chat_id, get_btc())
-            else:
-                continue
-    sleep(3)
-
-if __name__ == '__main__':
-    main()
+# def main():
+#     while True:
+#         answer = get_message()
+#
+#         if answer != None:
+#             chat_id = answer['chat_id']
+#             text = answer['text']
+#
+#             if text == '/btc':
+#                 send_message(chat_id, get_btc())
+#             else:
+#                 continue
+#     sleep(3)
+#
+# if __name__ == '__main__':
+#     main()
---
+++ main.py
@@ -0,0 +1,18 @@
+import bot
+
+def main():
+    while True:
+        answer = get_message()
+
+        if answer != None:
+            chat_id = answer['chat_id']
+            text = answer['text']
+
+            if text == '/btc':
+                send_message(chat_id, get_btc())
+            else:
+                continue
+    sleep(3)
+
+if __name__ == '__main__':
+    main()
